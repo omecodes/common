ca7d534e7865bc990d9596de4f4f9728a7483543
zoenion <zoenion.services@gmail.com>
2019-06-22 15:01:17 +0200

    Added migration feature Enhanced index management

--- dao/sql-v2.go
+++ dao/sql-v2.go
@@ -11,7 +11,8 @@ import (
 )
 
 const (
-	StatementKeyHasIndex = "has_index"
+	MySQLIndexScanner  = "mysql_index_scanner"
+	SQLiteIndexScanner = "sqlite_index_scanner"
 
 	VarPrefix        = "$prefix$"
 	VarAutoIncrement = "$auto_increment$"
@@ -29,6 +30,12 @@ type SQlv2Result struct {
 	AffectedRows int64
 }
 
+type SQLIndex struct {
+	Name   string
+	Table  string
+	Fields []string
+}
+
 type SQLv2 struct {
 	DB                   *sql.DB
 	dialect              string
@@ -36,7 +43,9 @@ type SQLv2 struct {
 	vars                 map[string]string
 	tableDefs            map[string]string
 	registeredStatements map[string]string
+	migrationScripts     []string
 	scanners             map[string]SQLv2RowScanner
+	initDone             bool
 }
 
 func (dao *SQLv2) Init(cfg conf.Map) error {
@@ -58,11 +67,8 @@ func (dao *SQLv2) Init(cfg conf.Map) error {
 		return errors.New("database dialect is not supported")
 	}
 
-	dao.AddStatement(StatementKeyHasIndex, ` SELECT COUNT(*)
-	FROM information_schema.statistics
-	WHERE TABLE_SCHEMA = DATABASE()
-  	AND TABLE_NAME = ? AND INDEX_NAME LIKE ?;`).
-		RegisterScanner(StatementKeyHasIndex, NewScannerFunc(dao.scanIndex))
+	dao.RegisterScanner(MySQLIndexScanner, NewScannerFunc(dao.mysqlIndexScan))
+	dao.RegisterScanner(SQLiteIndexScanner, NewScannerFunc(dao.sqliteIndexScan))
 
 	if dao.tableDefs != nil && len(dao.tableDefs) > 0 {
 		for _, schema := range dao.tableDefs {
@@ -90,6 +96,25 @@ func (dao *SQLv2) Init(cfg conf.Map) error {
 			dao.compiledStatements[name] = stmt
 		}
 	}
+	dao.initDone = true
+	return nil
+}
+
+func (dao *SQLv2) Migrate() error {
+	if !dao.initDone {
+		return errors.New("Init method must be called once before calling TableHasIndex method")
+	}
+
+	for _, ms := range dao.migrationScripts {
+		for name, value := range dao.vars {
+			ms = strings.Replace(ms, name, value, -1)
+		}
+
+		_, err := dao.DB.Exec(ms)
+		if err != nil {
+			return err
+		}
+	}
 	return nil
 }
 
@@ -109,7 +134,57 @@ func (dao *SQLv2) SetTablePrefix(prefix string) *SQLv2 {
 	return dao
 }
 
-func (dao *SQLv2) SetTableDefinition(name string, schema string) *SQLv2 {
+func (dao *SQLv2) AddMigrationScript(s string) *SQLv2 {
+	dao.migrationScripts = append(dao.migrationScripts, s)
+	return dao
+}
+
+func (dao *SQLv2) AddUniqueIndex(index SQLIndex, forceUpdate bool) error {
+	if !dao.initDone {
+		return errors.New("Init method must be called once before calling TableHasIndex method")
+	}
+
+	for varName, value := range dao.vars {
+		index.Table = strings.Replace(index.Table, varName, value, -1)
+	}
+	hasIndex, err := dao.TableHasIndex(index)
+	if err != nil {
+		return err
+	}
+
+	var result *SQlv2Result
+	if hasIndex && forceUpdate {
+		var dropIndexSQL string
+		if dao.dialect == "mysql" {
+			dropIndexSQL = fmt.Sprintf("drop index %s on %s", index.Name, index.Table)
+		} else {
+			dropIndexSQL = fmt.Sprintf("drop index if exists %s", index.Name)
+		}
+
+		result = dao.RawExec(dropIndexSQL)
+		if result.Error != nil {
+			return result.Error
+		}
+	}
+
+	if !hasIndex || forceUpdate {
+		var createIndexSQL string
+		if dao.dialect == "mysql" {
+			createIndexSQL = fmt.Sprintf("create unique index %s on %s(%s)", index.Name, index.Table, strings.Join(index.Fields, ","))
+		} else {
+			createIndexSQL = fmt.Sprintf("create unique index if not exists %s on %s(%s)", index.Name, index.Table, strings.Join(index.Fields, ","))
+		}
+
+		result = dao.RawExec(createIndexSQL)
+		if result.Error != nil {
+			return result.Error
+		}
+	}
+
+	return nil
+}
+
+func (dao *SQLv2) AddTableDefinition(name string, schema string) *SQLv2 {
 	if dao.tableDefs == nil {
 		dao.tableDefs = map[string]string{}
 	}
@@ -133,24 +208,42 @@ func (dao *SQLv2) RegisterScanner(name string, scanner SQLv2RowScanner) *SQLv2 {
 	return dao
 }
 
-func (dao *SQLv2) TableHasIndex(table string, indexName string) (bool, error) {
-	cursor, err := dao.Query(StatementKeyHasIndex, ScannerIndex, table, indexName)
+func (dao *SQLv2) TableHasIndex(index SQLIndex) (bool, error) {
+	if !dao.initDone {
+		return false, errors.New("Init method must be called once before calling TableHasIndex method")
+	}
+
+	var (
+		scannerName string
+		rawQuery    string
+	)
+	if dao.dialect == "mysql" {
+		rawQuery = fmt.Sprintf("SHOW INDEX FROM %s", index.Table)
+		scannerName = MySQLIndexScanner
+	} else {
+		rawQuery = fmt.Sprintf("PRAGMA INDEX_LIST('%s')", index.Table)
+		scannerName = SQLiteIndexScanner
+	}
+
+	cursor, err := dao.RawQuery(rawQuery, scannerName)
 	if err != nil {
 		return false, err
 	}
-
 	defer func() {
 		_ = cursor.Close()
 	}()
 
-	if cursor.HasNext() {
-		c, err := cursor.Next()
+	for cursor.HasNext() {
+		ind, err := cursor.Next()
 		if err != nil {
 			return false, err
 		}
-		return c.(int) > 0, nil
-	}
 
+		rowIndex := ind.(SQLIndex)
+		if rowIndex.Name == index.Name {
+			return true, nil
+		}
+	}
 	return false, nil
 }
 
@@ -218,13 +311,62 @@ func (dao *SQLv2) Exec(stmt string, params ...interface{}) *SQlv2Result {
 	return result
 }
 
-func (dao *SQLv2) scanIndex(rows *sql.Rows) (interface{}, error) {
-	var count int
-	err := rows.Scan(&count)
+func (dao *SQLv2) sqliteIndexScan(rows *sql.Rows) (interface{}, error) {
+	var index SQLIndex
+	m, err := dao.rowToMap(rows)
 	if err != nil {
-		count = 0
+		return nil, err
 	}
-	return count, nil
+
+	var ok bool
+	index.Name, ok = m["name"].(string)
+	if !ok {
+		return nil, errors.New("SQLite index scanned row 'Name' field type mismatches")
+	}
+	return index, nil
+}
+
+func (dao *SQLv2) mysqlIndexScan(rows *sql.Rows) (interface{}, error) {
+	var index SQLIndex
+	m, err := dao.rowToMap(rows)
+	if err != nil {
+		return nil, err
+	}
+
+	var ok bool
+	index.Name, ok = m["Key_name"].(string)
+	if !ok {
+		return nil, errors.New("mysql index scanned row 'Name' field type mismatches")
+	}
+	index.Table, ok = m["Table"].(string)
+	if !ok {
+		return nil, errors.New("mysql index scanned row 'Table' field type mismatches")
+	}
+	return index, nil
+}
+
+func (dao *SQLv2) rowToMap(rows *sql.Rows) (map[string]interface{}, error) {
+	cols, _ := rows.Columns()
+	columns := make([]interface{}, len(cols))
+	columnPointers := make([]interface{}, len(cols))
+	for i, _ := range columns {
+		columnPointers[i] = &columns[i]
+	}
+
+	// Scan the result into the column pointers...
+	if err := rows.Scan(columnPointers...); err != nil {
+		return nil, err
+	}
+
+	// Create our map, and retrieve the value for each column from the pointers slice,
+	// storing it in the map with the name of the column as the key.
+	m := make(map[string]interface{})
+	for i, colName := range cols {
+		val := columnPointers[i].(*interface{})
+		m[colName] = *val
+	}
+	return m, nil
+
 }
 
 func (dao *SQLv2) findCompileStatement(name string) (*sql.Stmt, error) {
