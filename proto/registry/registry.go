ffe0f2310d24d2c8585a5d68910034052dfc34de
zoenion <zoenion.services@gmail.com>
2019-08-05 22:08:25 +0200

    Added new service description Updated tree, registry

---
+++ proto/registry/registry.go
@@ -0,0 +1,13 @@
+package registrypb
+
+import (
+	"google.golang.org/grpc"
+)
+
+func NewClient(serverAddr string, opts ...grpc.DialOption) (RegistryClient, error) {
+	conn, err := grpc.Dial(serverAddr, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return NewRegistryClient(conn), nil
+}
---
+++ proto/tree/events.go
@@ -0,0 +1,24 @@
+package treepb
+
+type EventHandler interface {
+	OnEvent(event *Event) error
+}
+
+type handlerFunc struct {
+	f func(*Event) error
+}
+
+func (h *handlerFunc) OnEvent(event *Event) error {
+	return h.f(event)
+}
+
+func EventHandlerFunc(f func(*Event) error) EventHandler {
+	return &handlerFunc{
+		f: f,
+	}
+}
+
+type Watcher interface {
+	Watch(EventHandler) error
+	Stop() error
+}
