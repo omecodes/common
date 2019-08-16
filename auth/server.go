38914784811de62d9b87c5770bf8f25712de83aa
zoenion <zoenion.services@gmail.com>
2019-08-16 22:35:16 +0200

    Added token verifier interface and default implementation

--- auth/server.go
+++ auth/server.go
@@ -12,66 +12,6 @@ import (
 	"time"
 )
 
-type gRPCClientApiAccessCredentials struct {
-	key, secret string
-}
-
-func (g *gRPCClientApiAccessCredentials) GetRequestMetadata(ctx context.Context, uri ...string) (map[string]string, error) {
-	return map[string]string{
-		"authorization": fmt.Sprintf("Access %s:%s", g.key, g.secret),
-	}, nil
-}
-
-func (g *gRPCClientApiAccessCredentials) RequireTransportSecurity() bool {
-	return true
-}
-
-func NewGRPCClientApiAccessCredentials(key, secret string) *gRPCClientApiAccessCredentials {
-	return &gRPCClientApiAccessCredentials{
-		key:    key,
-		secret: secret,
-	}
-}
-
-type gRPCClientBasicAuthentication struct {
-	user, password string
-}
-
-func (g *gRPCClientBasicAuthentication) GetRequestMetadata(ctx context.Context, uri ...string) (map[string]string, error) {
-	return map[string]string{
-		"authorization": fmt.Sprintf("Basic %s:%s", g.user, g.password),
-	}, nil
-}
-
-func (g *gRPCClientBasicAuthentication) RequireTransportSecurity() bool {
-	return true
-}
-
-func NewGRPCBasicAuthentication(user, password string) *gRPCClientBasicAuthentication {
-	return &gRPCClientBasicAuthentication{
-		user:     user,
-		password: password,
-	}
-}
-
-type gRPCClientTokenAuth struct {
-	token string
-}
-
-func (g *gRPCClientTokenAuth) GetRequestMetadata(ctx context.Context, uri ...string) (map[string]string, error) {
-	return map[string]string{
-		"authorization": "Bearer " + g.token,
-	}, nil
-}
-
-func (g *gRPCClientTokenAuth) RequireTransportSecurity() bool {
-	return true
-}
-
-func NewGRPCClientTokenAuthentication(t string) *gRPCClientTokenAuth {
-	return &gRPCClientTokenAuth{token: t}
-}
-
 //
 type gRPCServerAccessAuthentication struct {
 	access  string
---
+++ proto/auth/verifier.go
@@ -0,0 +1,154 @@
+package authpb
+
+import (
+	"context"
+	"crypto/ecdsa"
+	"crypto/sha256"
+	"crypto/x509"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"math/big"
+	"strings"
+	"sync"
+	"time"
+
+	"github.com/zoenion/common/errors"
+)
+
+type TokenVerifier interface {
+	Verify(ctx context.Context, t *Token) (TokenState, error)
+}
+
+type tokenVerifier struct {
+	sync.Mutex
+	certificate *x509.Certificate
+}
+
+func (v *tokenVerifier) verifySignature(t *Token) (bool, error) {
+	if t.Claims == nil {
+		return false, errors.Forbidden
+	}
+	parts := strings.Split(t.Signature, ":")
+	r, err := base64.StdEncoding.DecodeString(parts[0])
+	if err != nil {
+		return false, errors.New("token wrong format")
+	}
+	s, err := base64.StdEncoding.DecodeString(parts[1])
+	if err != nil {
+		return false, errors.New("token wrong format")
+	}
+
+	rInt := &big.Int{}
+	rInt.SetBytes(r)
+	sInt := &big.Int{}
+	sInt.SetBytes(s)
+
+	claimsBytes, err := json.Marshal(t.Claims)
+	if err != nil {
+		return false, fmt.Errorf("could not encode claims: %s", err)
+	}
+
+	sha := sha256.New()
+	sha.Write(claimsBytes)
+	hash := sha.Sum(nil)
+
+	if t.Header.Alg == "ecdsa" {
+		key, ok := v.certificate.PublicKey.(*ecdsa.PublicKey)
+		if !ok {
+			return false, errors.New("could not verify token with the public key")
+		}
+		return ecdsa.Verify(key, hash, rInt, sInt), nil
+	} else {
+		return false, errors.NotSupported
+	}
+}
+
+func (v *tokenVerifier) verifyToken(ctx context.Context, t *Token) (TokenState, error) {
+	verified, err := v.verifySignature(t)
+	if err != nil {
+		return 0, err
+	}
+
+	if !verified {
+		return TokenState_NOT_SIGNED, nil
+	}
+
+	if t.Claims.Exp != -1 && t.Claims.Exp <= time.Now().Unix() {
+		return TokenState_EXPIRED, nil
+	}
+
+	if t.Claims.Nbf != -1 && t.Claims.Nbf > time.Now().Unix() {
+		return TokenState_NOT_EFFECTIVE, nil
+	}
+
+	return TokenState_VALID, nil
+}
+
+func (v *tokenVerifier) Verify(ctx context.Context, t *Token) (TokenState, error) {
+	if t == nil {
+		return TokenState_NOT_VALID, errors.Forbidden
+	}
+
+	s, err := v.verifyToken(ctx, t)
+	if err != nil {
+		return TokenState_NOT_VALID, errors.Forbidden
+	}
+	return s, nil
+}
+
+func NewTokenVerifier(certificate *x509.Certificate) *tokenVerifier {
+	return &tokenVerifier{
+		certificate: certificate,
+	}
+}
+
+func TokenFromJWT(jwt string) (*Token, error) {
+	if jwt == "" {
+		return nil, nil
+	}
+
+	jwt = strings.Replace(jwt, "Bearer ", "", 1)
+
+	malformed := errors.New("malformed token")
+	parts := strings.Split(jwt, ".")
+	if len(parts) != 3 {
+		return nil, errors.New("missing parts")
+	}
+
+	var t Token
+	t.Header = new(TokenHeader)
+	t.Claims = new(Claims)
+
+	headerBytes, _ := base64.StdEncoding.DecodeString(parts[0])
+	if headerBytes == nil {
+		return nil, malformed
+	}
+
+	claimsBytes, _ := base64.StdEncoding.DecodeString(parts[1])
+	if claimsBytes == nil {
+		return nil, malformed
+	}
+
+	signatureBytes, _ := base64.StdEncoding.DecodeString(parts[2])
+	if signatureBytes == nil {
+		return nil, malformed
+	}
+
+	err := json.Unmarshal(headerBytes, t.Header)
+	if err != nil {
+		return nil, malformed
+	}
+
+	err = json.Unmarshal(claimsBytes, t.Claims)
+	if err != nil {
+		return nil, malformed
+	}
+
+	err = json.Unmarshal(signatureBytes, &t.Signature)
+	if err != nil {
+		return nil, malformed
+	}
+
+	return &t, nil
+}
