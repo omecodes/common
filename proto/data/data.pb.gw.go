d287d9207f425d8b619c9d4f8260a761afba9166
zoenion <zoenion.services@gmail.com>
2019-10-26 17:07:32 +0200

    Updated data service definition

--- proto/data/data.pb.gw.go
+++ proto/data/data.pb.gw.go
@@ -62,6 +62,23 @@ func request_DataStore_GetAttributes_0(ctx context.Context, marshaler runtime.Ma
 
 }
 
+func request_DataStore_DeleteAttributes_0(ctx context.Context, marshaler runtime.Marshaler, client DataStoreClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq DeleteAttributesRequest
+	var metadata runtime.ServerMetadata
+
+	newReader, berr := utilities.IOReaderFactory(req.Body)
+	if berr != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
+	}
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := client.DeleteAttributes(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
 // RegisterDataStoreHandlerFromEndpoint is same as RegisterDataStoreHandler but
 // automatically dials to "endpoint" and closes the connection when "ctx" gets done.
 func RegisterDataStoreHandlerFromEndpoint(ctx context.Context, mux *runtime.ServeMux, endpoint string, opts []grpc.DialOption) (err error) {
@@ -140,17 +157,41 @@ func RegisterDataStoreHandlerClient(ctx context.Context, mux *runtime.ServeMux, 
 
 	})
 
+	mux.Handle("POST", pattern_DataStore_DeleteAttributes_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateContext(ctx, mux, req)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_DataStore_DeleteAttributes_0(rctx, inboundMarshaler, client, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_DataStore_DeleteAttributes_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	return nil
 }
 
 var (
-	pattern_DataStore_SetAttributes_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"data", "save"}, ""))
+	pattern_DataStore_SetAttributes_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"data", "set"}, ""))
 
-	pattern_DataStore_GetAttributes_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0}, []string{"data"}, ""))
+	pattern_DataStore_GetAttributes_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"data", "get"}, ""))
+
+	pattern_DataStore_DeleteAttributes_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"data", "delete"}, ""))
 )
 
 var (
 	forward_DataStore_SetAttributes_0 = runtime.ForwardResponseMessage
 
 	forward_DataStore_GetAttributes_0 = runtime.ForwardResponseMessage
+
+	forward_DataStore_DeleteAttributes_0 = runtime.ForwardResponseMessage
 )
