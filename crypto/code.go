7f7a233599f29f43b3b148d2d6e160ea85398e46
zoenion <zoenion.services@gmail.com>
2020-05-29 00:51:15 +0200

    Added random code generate function

--- crypto/code.go
+++ crypto/code.go
@@ -2,6 +2,7 @@ package crypto2
 
 import (
 	"crypto/rand"
+	"encoding/hex"
 	"io"
 )
 
@@ -12,10 +13,23 @@ func GenerateVerificationCode(max int) (string, error)  {
 	b := make([]byte, max)
 	n, err := io.ReadAtLeast(rand.Reader, b, max)
 	if n != max {
-		panic(err)
+		return "", nil
+	}
+
+	if err != nil {
+		return "", err
 	}
 	for i := 0; i < len(b); i++ {
 		b[i] = byte(verificationCodeDigits[int(b[i])%len(verificationCodeDigits)])
 	}
 	return string(b), nil
+}
+
+func RandomCode(max int) (string, error) {
+	buff := make([]byte, max)
+	_, err := rand.Read(buff)
+	if err != nil {
+		return "", err
+	}
+	return hex.EncodeToString(buff), nil
 }
