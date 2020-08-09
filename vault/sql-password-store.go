57e58f826c6537b7eb95c397c159393bb3f2479a
omecodes <omecodes@gmail.com>
2020-08-09 13:04:26 +0200

    Code refactoring

--- vault/sql-password-store.go
+++ vault/sql-password-store.go
@@ -1,9 +1,7 @@
 package vault
 
 import (
-	"crypto/sha256"
 	"database/sql"
-	"encoding/hex"
 	"github.com/omecodes/common/codec"
 	"github.com/omecodes/common/errors"
 	"github.com/omecodes/common/jcon"
@@ -18,13 +16,8 @@ type passwordStore struct {
 	db dict.Dict
 }
 
-func (p *passwordStore) hexHashed(password string) string {
-	hashedPasswordBytes := sha256.Sum256([]byte(password))
-	return hex.EncodeToString(hashedPasswordBytes[:])
-}
-
 func (p *passwordStore) Save(username, password string) error {
-	return p.db.Save(username, &stringWrapper{Value: p.hexHashed(password)})
+	return p.db.Save(username, &stringWrapper{Value: password})
 }
 
 func (p *passwordStore) Update(username, oldPassword, newPassword string) error {
@@ -51,12 +44,11 @@ func (p *passwordStore) Delete(username string) error {
 }
 
 func (p *passwordStore) Verify(username, password string) (bool, error) {
-	hexPassword := p.hexHashed(password)
 	savedPassword, err := p.Get(username)
 	if err != nil {
 		return false, err
 	}
-	return hexPassword == savedPassword, nil
+	return password == savedPassword, nil
 }
 
 func NewPasswordStore(cfg jcon.Map, tableNamePrefix string) (*passwordStore, error) {
