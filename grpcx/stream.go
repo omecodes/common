bad729714f35046b46e893696b2a6b36c48728d9
omecodes <omecodes@gmail.com>
2020-08-17 23:27:33 +0200

    Added logging

--- grpcx/stream.go
+++ grpcx/stream.go
@@ -2,6 +2,7 @@ package grpcx
 
 import (
 	"context"
+	"github.com/omecodes/common/utils/log"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/metadata"
 )
@@ -23,6 +24,7 @@ func (g *gRPCServerStreamContextWrapper) SetHeader(m metadata.MD) error {
 }
 
 func (g *gRPCServerStreamContextWrapper) SendHeader(m metadata.MD) error {
+	log.Info("[grpc::stream] send", log.Field("header", m))
 	return g.wrapped.SendHeader(m)
 }
 
@@ -38,9 +40,11 @@ func (g *gRPCServerStreamContextWrapper) Context() context.Context {
 }
 
 func (g *gRPCServerStreamContextWrapper) SendMsg(m interface{}) error {
+	log.Info("[grpc::stream] send", log.Field("msg", m))
 	return g.wrapped.SendMsg(m)
 }
 
 func (g *gRPCServerStreamContextWrapper) RecvMsg(m interface{}) error {
+	log.Info("[grpc::stream] recv", log.Field("msg", m))
 	return g.wrapped.RecvMsg(m)
 }
