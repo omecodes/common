6fa8b46d445b35a9292fbc962952db446c973514
omecodes <omecodes@gmail.com>
2020-11-29 01:01:46 +0100

    Removed unused codes

--- security/vault/password-store.go
+++
@@ -1,9 +0,0 @@
-package vault
-
-type PasswordStore interface {
-	Save(username, password string) error
-	Update(username, odlPassword, newPassword string) error
-	Get(username string) (string, error)
-	Delete(username string) error
-	Verify(username, password string) (bool, error)
-}
